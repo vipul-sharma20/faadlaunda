52e3c343c5c37a71ec154bc6ed109b027fcaeb29
Vipul <vipul.sharma20@gmail.com>
2017-07-21 03:22:10 +0530

    add personilities and trait

--- view.go
+++ view.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-    "io/ioutil"
-    "net/http"
-    "html/template"
     "encoding/xml"
+    "html/template"
+    "io/ioutil"
+    "math/rand"
+    "net/http"
+    "time"
 )
 
 type Context struct {
     Url string `xml:"data>images>image>url"`
+    Trait string
+    Person string
 }
 
 func catRequest() []byte {
@@ -17,7 +21,7 @@ func catRequest() []byte {
     query_string := request.URL.Query()
     query_string.Add("type", "jpg,png")
     query_string.Add("format", "html")
-    query_string.Add("size", "med,full")
+    query_string.Add("size", "med")
     query_string.Add("format", "xml")
 
     request.URL.RawQuery = query_string.Encode()
@@ -31,6 +35,10 @@ func buildTemplate(w http.ResponseWriter, r *http.Request) {
     t, _ := template.ParseFiles("cats.html")
     body := catRequest()
     var context Context
+
+    rand.Seed(time.Now().Unix())
+    context.Trait = traits[rand.Intn(len(traits))]
+    context.Person = personalities[rand.Intn(len(personalities))]
 
     xml.Unmarshal(body, &context)
     t.Execute(w, context)
